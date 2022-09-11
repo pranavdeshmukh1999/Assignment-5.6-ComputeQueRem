61fa894e0027c219576f21f88608f189dcf6e8d0
pranavdeshmukh1999 <pranavdeshmukh182@gmail.com>
2022-09-11 08:32:25 +0530

    Added :Compute QUetient & Reminder

---
+++ src/com/QueRem/ComputeQueRem.java
@@ -0,0 +1,14 @@
+package com.QueRem;
+
+public class ComputeQueRem {
+
+	public static void main(String[] args) {
+		int dividend = 100, divisor = 20;
+
+		int quotient = dividend / divisor;
+		int remainder = dividend % divisor;
+
+		System.out.println("Quotient is = " + quotient);
+		System.out.println("Remainder is = " + remainder);
+	}
+}
